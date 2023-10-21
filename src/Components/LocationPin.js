d8237ab0f1853d0a60335bacdcd7aea8e516c570
ahmeddba <ahmed.benabid@esprit.tn>
2023-10-21 10:41:00 +0100

    Google Maps

---
+++ src/Components/LocationPin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LocationPin = () => {
+
+    const location = {
+        address: '1600 Amphitheatre Parkway, Mountain View, california.',
+        lat: 37.42216,
+        lng: -122.08427,
+      }
+  return (
+    <div>LocationPin</div>
+  )
+}
+
+export default LocationPin
